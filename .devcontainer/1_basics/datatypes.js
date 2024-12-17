73f88194be16eefcee4b80e4e49ca7f8af20eaf9
Saad Nabi <147240312+ZionRDS-220@users.noreply.github.com>
2024-12-17 19:36:36 +0000

    datatypes

---
+++ .devcontainer/1_basics/datatypes.js
@@ -0,0 +1,18 @@
+ "use strict"; //treat all js code as newer version
+
+// alert (3+3)  // we are using node js , not browser
+
+console.log(3 + 3)
+
+//databtypes
+//number => 2 to 52
+//string => ""
+//boolean => true/false
+//bigint = for writing big values
+//null => standalone value
+//undefined => vlue not assigned
+//symbol => use for uniqueness
+//object
+
+console.log (typeof undefined); // undefined
+console.log (typeof null); //object
