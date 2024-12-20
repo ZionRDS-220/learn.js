424664f9fc000c653e789565d2b60d676fa621d9
Saad Nabi <147240312+ZionRDS-220@users.noreply.github.com>
2024-12-20 10:08:50 +0000

    conversionOperation

--- .devcontainer/1_basics/conversionOperation.js
+++ .devcontainer/1_basics/conversionOperation.js
@@ -1,6 +1,6 @@
-let score = 33
+//let score = 33
 
-console.log(typeof score);
+//console.log(typeof score);
 
 //"33" => 33
 //"33abc" => NaN (not an number)
@@ -8,3 +8,10 @@ console.log(typeof score);
 //false => 0
 //"" => false
 //"Saad" => true
+
+// ******** Operations******* 
+
+let str1 = "hello"
+let str2 = " Saad"
+let str3 = str1 + str2
+console.log(str3);
