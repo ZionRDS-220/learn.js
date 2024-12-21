1268ab93b28adcb976f6656732428a34f9c46683
Saad Nabi <147240312+ZionRDS-220@users.noreply.github.com>
2024-12-21 20:01:36 +0000

    datatype summary

---
+++ .devcontainer/1_basics/datatypesSummary.js
@@ -0,0 +1,33 @@
+// how data store in the memory are all data types that are devided into two parts 
+// => primitive and non-premitive
+
+// Primitive data types
+// 7 types: are call by value
+// string, number, boolean, null, undefined, symbol, bigInt
+
+
+
+// Reference data types (non-premitive)
+// array, objects, functions
+
+// Arrays
+// arrays are written in square brackets [] and define its value
+// e.g, const heros = [supraman, batman, kraven]
+
+// Objects
+// objects are written in curly brackets {} with key value pairs
+// e.g,
+// {
+// name; " Saad"
+// Age ; "22"
+// }
+
+// functions
+// function(){
+//console.log("hello world")
+//}
+
+//how to know the datatype of any value
+// console.log(typeof name of any variable);
+
+// the return type of non-primitive datatype is (object) and of the function is (object function)
